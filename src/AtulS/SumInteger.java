56c134ec3827f7409678feefaf0f111eea0b4fe6
Atul Shrivastava <ashrivastava14@gmail.com>
2019-08-24 17:56:21 +0530

    Added program Sum of two integer

---
+++ src/AtulS/SumInteger.java
@@ -0,0 +1,13 @@
+//program to print the sum of two integers
+class SumInteger
+{
+	int sum(int x,int y)
+	{
+		return x+y;
+	}
+	
+	public static void main(String a[])
+	{
+		System.out.println(new SumInteger().sum(15,13));
+	}
+}
