e4d0f71ee1c0b4766b488c09a478bd7b84faf532
shreya dubey <shreyadubey1897@gmail.com>
2019-08-24 20:57:11 +0530

    adding assignment ques5

---
+++ src/ShreyaD/Question5.java
@@ -0,0 +1,16 @@
+package ShreyaD;
+
+public class Question5 {
+	void display(int start,int end){
+		for(int i=start;i<=end;i++){
+			System.out.println(i);
+		}
+		
+	}
+    public static void main(String[] args) {
+		Question5 question = new Question5();
+		question.display(2,80);
+		
+	}
+
+}
