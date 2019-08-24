2ff0d2a4772c036dd900a9c9e7449799eb8f597e
VarshaTAug19 <varshatiwari1408@gmail.com>
2019-08-24 20:52:18 +0530

    assignment 1 question5 solution

---
+++ src/VarshaT/Ques5.java
@@ -0,0 +1,15 @@
+package VarshaT;
+
+public class Ques5 {
+	void print(int start, int end) {
+		for (int i = start; i <= end; i++) {
+			System.out.println(i);
+		}
+	}
+
+	public static void main(String[] args) {
+		Ques5 ques5 = new Ques5();
+		ques5.print(5, 10);
+	}
+
+}
