acef944bdc12828c2a91be41782c879bc0b858d5
VarshaTAug19 <varshatiwari1408@gmail.com>
2019-08-25 19:42:52 +0530

    assignment2 ques1

--- src/VarshaT/Asngmt2_Ques1.java
+++ src/VarshaT/Asngmt2_Ques1.java
@@ -4,7 +4,7 @@ public class Asngmt2_Ques1 {
 	void missing(int a[], int b[]) {
 
 		for (int i = 0; i < a.length; i++) {
-			int flag = 0;
+			int flag = 0; //when element is present in array flag=1
 			for (int j = 0; j < b.length; j++) {
 				if (a[i]==b[j]) {
 					flag++;
@@ -12,7 +12,7 @@ public class Asngmt2_Ques1 {
 				}
 			}
 			if (flag == 0)
-				System.out.println(a[i] + "is missing");
+				System.out.println(a[i] + "is missing"); //printing missing elements
 		}
 
 	}
@@ -20,8 +20,8 @@ public class Asngmt2_Ques1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Asngmt2_Ques1 asngmt2_Ques1 = new Asngmt2_Ques1();
-		int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9,10 };
-		int b[] = { 1, 2, 4, 6, 4, 3 };
+		int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9,10 };//array to be compared with
+		int b[] = { 1, 2, 4, 6, 4, 3 };//input array with missing elements
 		asngmt2_Ques1.missing(a, b);
 
 	}
