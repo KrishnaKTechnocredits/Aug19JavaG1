c0704f0f79f45294c7bf61ff6b39737f892f15f2
Snehal <snehal.k.chaudhari@gmail.com>
2019-08-25 11:49:14 +0530

    adding SumInteger program

---
+++ src/SnehalW/SumInt.java
@@ -0,0 +1,17 @@
+package SnehalW;
+class SumInt
+{
+	int sum=0,a,b;
+	
+	int sum(int a,int b)
+	{	
+		sum = a+b;	
+		return sum;
+	}
+	public static void main(String a[])
+	{
+		SumInt sumint = new SumInt();
+		int add = sumint.sum(5,6);			
+		System.out.println("The Sum of two integers is : " +add);
+	}
+}
