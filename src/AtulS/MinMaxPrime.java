83b534e03a4a82fcbf45b5d7cce10f938660cebc
Atul Shrivastava <ashrivastava14@gmail.com>
2019-08-26 15:20:34 +0530

    Added minmax prime java program

--- src/AtulS/MinMaxPrime.java
+++ src/AtulS/MinMaxPrime.java
@@ -1,6 +1,5 @@
 package AtulS;
 
-
 //program to identify near minimum and maximum prime number of a given number
 //e.g. For number 10 near min prime# is 7 and max prime# is 11
 
@@ -10,37 +9,37 @@ public class MinMaxPrime {
 		int a = 2;
 		int cnt = 0;
 		int k = 0;
-		for (int j = num - 1; j >= 2; j--) {
-			cnt = 0;
-			for (int i = 2; i <= j / 2; i++) {
-				if (j % i == 0) {
+//		for (int j = num - 1; j >= 2; j--) {
+//			cnt = 0;
+//			for (int i = 2; i <= j / 2; i++) {
+//				if (j % i == 0) {
+//					cnt = 1;
+//					break;
+//				}
+//			}
+//
+//			if (cnt == 0) {
+//				System.out.println("Min Number " + j + " is prime");
+//				break;
+//			}
+//		}
+
+		for (k = num + 1; ; k++) {
+			cnt=0;
+			for (int l = 2; l < k / 2; l++) {
+				if (k % l == 0) {
 					cnt = 1;
-					break;
 				}
 			}
-
-			if (cnt == 0) {
-				System.out.println("Min Number " + j + " is prime");
-				break;
-			}
-		}
-		
-		for (k = num + 1; k < 999; k++) {
-			cnt = 0;
-			if (k % a == 0) {
-				cnt = 1;
-			}
-			a++;
 			if (cnt == 0) {
 				System.out.println("Max Number " + k + " is prime");
 				break;
 			}
 		}
-
 	}
 
 	public static void main(String[] args) {
-		primeminmax(25);
+		primeminmax(50);
 	}
 
 }
