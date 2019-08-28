1b1535b68a7706452e1e9e3b83302454491d0706
Atul Shrivastava <ashrivastava14@gmail.com>
2019-08-28 20:03:35 +0530

    Added program of counting of vowels for Java first assignment

--- src/AtulS/VowelCount.java
+++ src/AtulS/VowelCount.java
@@ -2,19 +2,19 @@ package AtulS;
 
 public class VowelCount {
 
-	void printVowelCount(String input, char ch) {
+	void printVowelCount(String input) {
 		int count = 0;
 		for (int i = 0; i < input.length(); i++) {
-			if (input.charAt(i) == ch) {
+			if (input.charAt(i) == 'a'||input.charAt(i) == 'e'||input.charAt(i) == 'i'||input.charAt(i) == 'o'||input.charAt(i) == 'u') {
 				count = count + 1;
 			}
 		}
-		System.out.println(ch + ":"+count);
+		System.out.println("Vowels count in given string is" + ":"+count);
 	}
 
 	public static void main(String[] args) {
 		VowelCount vowelcnt = new VowelCount();
-		vowelcnt.printVowelCount("atulShrivastava", 'a');
+		vowelcnt.printVowelCount("atulshrivastava");
 
 	}
 
