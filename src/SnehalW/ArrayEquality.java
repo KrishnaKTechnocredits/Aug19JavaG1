f917b6cf3f3cc12fe2572053bc0b8ad977a8c5c5
SnehalWAug19 <snehal.k.chaudhari@gmail.com>
2019-08-29 22:44:19 +0530

    Adding ArrayEquality and Arraysum7 program

---
+++ src/SnehalW/ArrayEquality.java
@@ -0,0 +1,34 @@
+package SnehalW;
+
+public class ArrayEquality 
+{
+	int array1[] = {21, 57, 11, 37, 24};
+	int array2[] = {21, 57, 11, 37, 24};
+	int count;
+	void equality()
+	{
+		for(int i=0;i<array1.length;i++)
+		{
+			for(int j=0;j<array2.length;j++)
+			{
+				if(array1.length ==array2.length && array1[i]==array2[j])
+				{
+					count++;
+				}
+			}
+		}
+		if(array1.length == count)
+		System.out.println("Both the arrays are equals");
+		else
+			System.out.println("Both array are not equal");
+			
+	}
+	public static void main(String[] args) 
+	{
+		ArrayEquality arrayequality = new ArrayEquality();
+		arrayequality.equality();
+		
+
+	}
+
+}
