468f26ec52876cc6e2ced4177843372190b48cc5
Manoj <manoj.nargade@gmail.com>
2019-08-31 19:43:05 +0530

    Adding Assignments-2 all  questions

---
+++ src/ManojN/Q13_reveres_array.java
@@ -0,0 +1,15 @@
+/*
+Q13) WAP to reveres array
+a. int inputArray = {5, 6, 9, 10, 14, 63}
+b. outputArray = {63, 14, 10, 9, 6, 5}
+ */
+package a2;
+public class Q13_reveres_array {
+
+	public static void main(String[] args) {
+		int a[]={5, 6, 9, 10, 14, 63};
+		for (int i=(a.length-1);i>=0; i--){
+			System.out.print(a[i]+",");
+		}
+	}
+}
