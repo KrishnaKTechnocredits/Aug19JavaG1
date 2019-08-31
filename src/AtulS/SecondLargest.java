4320b9bf1e70973fae6ab1f269cf99f406342349
Atul Shrivastava <ashrivastava14@gmail.com>
2019-08-31 10:15:33 +0530

    Updated changes

--- src/AtulS/SecondLargest.java
+++ src/AtulS/SecondLargest.java
@@ -5,7 +5,7 @@ public class SecondLargest {
 	void secondlargestnum(int[] arr)
 	{
 		int frstlargest  = arr[0];
-		int secondlargest = arr[0];
+		int secondlargest = 0;
 		for(int i = 0;i<arr.length;i++)
 		{
 			if(arr[i] > frstlargest)
@@ -13,7 +13,7 @@ public class SecondLargest {
 				secondlargest = frstlargest;
 				frstlargest = arr[i];
 			}
-			else if(arr[i] > secondlargest)
+			else if(arr[i] > secondlargest & frstlargest > arr[i])
 			{
 				secondlargest = arr[i];
 			}
@@ -21,7 +21,7 @@ public class SecondLargest {
 		System.out.println("Second Largest number in given array is " + secondlargest);
 	}
 	public static void main(String[] args) {
-		int[] arr = {1,2,3,4};
+		int[] arr = {16,2,3,4};
 		SecondLargest sc = new SecondLargest();
 		sc.secondlargestnum(arr);
 		
