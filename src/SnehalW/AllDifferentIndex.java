8a9c05542edcc97982a29e39d8b295dd477fd485
SnehalWAug19 <snehal.k.chaudhari@gmail.com>
2019-09-01 20:36:00 +0530

    Updated All Diferentarray program

--- src/SnehalW/AllDifferentIndex.java
+++ src/SnehalW/AllDifferentIndex.java
@@ -1,28 +1,25 @@
 package SnehalW;
+
 //9) Find all differ index from given arrays
+
 public class AllDifferentIndex {
 
 	int array1[] = {10,2,9,14,3};
 	int array2[] = {10,2,18,14,3};
 	int i ,j;
-		void differIndex()
-		{
-			for(i=0;i<array1.length;i++)
+	void differIndex()
+	{
+		for(i=0;i<array1.length;i++)
+		{	
+			if(array1[i]!=array2[i])
 			{
-				for(j=i;j<array2.length;j++)
-				{
-					if(array1[i]!=array2[j])
-					{
-						System.out.println("The arrays is not equal at the index : "+i +" is "+array1[i]+","+array2[j]);			
-					}
-					break;
-				}
+				System.out.println("The arrays is not equal at the index : "+i +" is "+array1[i]+","+array2[i]);			
 			}
 		}
+	}
 		
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		AllDifferentIndex alldifferentindex = new AllDifferentIndex();
 		alldifferentindex.differIndex();
 	}
-
 }
