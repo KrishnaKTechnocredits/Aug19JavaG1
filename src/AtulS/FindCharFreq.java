e0b9eca62430160630b9f38cab9195a56e275799
Atul Shrivastava <ashrivastava14@gmail.com>
2019-09-06 21:54:47 +0530

    Added program

--- src/AtulS/FindCharFreq.java
+++ src/AtulS/FindCharFreq.java
@@ -5,26 +5,23 @@ import java.util.*;
 
 public class FindCharFreq {
 
-	int findchar(String str, char ch) { //Query in this function:not working for string elee
+	int findchar(String str, char ch) { // Query in this function:not working
+										// for string elee
 		int cnt = 0;
-		for(int i=0;i<str.length();i++)
-		{
-			if(str.charAt(i)==ch)
-			{
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == ch) {
 				cnt = cnt + 1;
 			}
 		}
 		return cnt;
 	}
-	
-	
 
 	public static void main(String[] args) {
-		String str;
 		char ch;
+		String str;
 		FindCharFreq fc = new FindCharFreq();
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter string:");
+		System.out.println("Enter word");
 		str = sc.nextLine();
 		System.out.println("Enter char which you want to find:");
 		ch = sc.next().charAt(0);
