85042de48883d8b69c09637b1cc19148ec6268dc
Manoj <manoj.nargade@gmail.com>
2019-09-08 12:07:59 +0530

    adding P1 and P2

---
+++ src/ManojN/P1.java
@@ -0,0 +1,26 @@
+package ManojN;
+
+
+
+public class P1 {
+	public static void main(String[] args) {
+		for (int i=1; i<=4; i++){
+			for (int j=1; j<=i; j++){
+				int v=0;
+				if (j==1)
+				System.out.print(i + " ");
+				else { 
+					v= i+(j-1) *2; 
+					System.out.print(v+ " ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
+/*
+	1 
+	2 4 
+	3 5 7 
+	4 6 8 10 
+*/
