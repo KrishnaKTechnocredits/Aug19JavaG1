e6663a09a8adeb70e90209d2e334eead198bd3e5
ShreyaDAug19 <shreyadubey1897@gmail.com>
2019-09-08 12:07:05 +0530

    adding file p1

---
+++ src/ShreyaD/p1.java
@@ -0,0 +1,23 @@
+package ShreyaD;
+
+import java.util.Scanner;
+
+public class p1 {
+	void pattern(int n){
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		System.out.println("please enter the no");
+		int n = sc.nextInt();
+		p1 p = new p1();
+		p.pattern(n);
+		sc.close();
+
+}
+}
