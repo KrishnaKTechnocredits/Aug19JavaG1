4a10af53b972ca3e3443ddb5c6463eacdc44051f
VishakhapAug19 <vishpagar28@gmail.com>
2019-09-08 12:08:37 +0530

    adding p1

---
+++ src/VishakhaP/P1.java
@@ -0,0 +1,19 @@
+package VishakhaP;
+
+public class P1 {
+		public static void main(String args[])
+		{
+			for(int i =1;i<=4 ;i++)
+			{
+				for(int j = 1;j<= i;j++)
+				{
+					System.out.println("* ");
+				}
+				    System.out.println("") ;  
+			}
+		}
+
+	}
+
+
+
