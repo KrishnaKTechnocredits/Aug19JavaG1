3ddec2ef993883d56b9ac2cac809aaa30450c1b4
Atul Shrivastava <ashrivastava14@gmail.com>
2019-09-13 20:38:28 +0530

    Added P10 file

---
+++ src/AtulS/P10.java
@@ -0,0 +1,14 @@
+package AtulS;
+
+public class P10 {
+
+	static void display()
+	{
+		System.out.println("This is conflict issue in GIT");
+	}
+	public static void main(String[] args) {
+		display();
+
+	}
+
+}
