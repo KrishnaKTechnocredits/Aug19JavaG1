ade524878c1b777bcfe6baa29e038b3a2f09429b
SnehalWAug19 <snehal.k.chaudhari@gmail.com>
2019-09-13 21:41:35 +0530

    Adding p10 file

---
+++ src/SnehalW/P10.java
@@ -0,0 +1,22 @@
+package SnehalW;
+
+import java.util.Scanner;
+
+public class P10 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the no of rows: ");
+		int row = sc.nextInt();
+		sc.close();
+		
+		for(int i=1; i<=row; i++)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
