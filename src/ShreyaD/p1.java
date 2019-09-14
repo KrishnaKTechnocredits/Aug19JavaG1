7f803b1c15b1be6a036f50c1d87465f8d8b501d4
ShreyaDAug19 <shreyadubey1897@gmail.com>
2019-09-14 19:26:12 +0530

    adding pattern program

--- src/ShreyaD/p1.java
+++ src/ShreyaD/p1.java
@@ -2,22 +2,22 @@ package ShreyaD;
 
 import java.util.Scanner;
 
-public class p1 {
-	void pattern(int n){
-		for(int i=1;i<=n;i++){
-			for(int j=1;j<=i;j++){
-				System.out.print("*");
-			}
-			System.out.println();
+public class P1 {
+	void pattern(int n)
+	{
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=i; j++){
+			System.out.print("*");
 		}
+		System.out.println();
 	}
-	public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
+}
+	public static void main(String[] args) {	
+	 Scanner sc = new Scanner(System.in);
 		System.out.println("please enter the no");
 		int n = sc.nextInt();
-		p1 p = new p1();
+		P1 p = new P1();
 		p.pattern(n);
 		sc.close();
-
 }
 }
