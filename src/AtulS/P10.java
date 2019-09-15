4ffaaf1d3b6c2714518511905a2162dd56ba4aec
Atul Shrivastava <ashrivastava14@gmail.com>
2019-09-15 22:47:24 +0530

    Added P10

---
+++ src/AtulS/P10.java
@@ -0,0 +1,14 @@
+package AtulS;
+
+public class P10 {
+void m1()
+{
+	System.out.println("This is conflict");
+}
+	public static void main(String[] args) {
+		 P10 p = new P10();
+		 p.m1();
+
+	}
+
+}
