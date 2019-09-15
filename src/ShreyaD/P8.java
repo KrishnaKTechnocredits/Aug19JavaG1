2a5450400ee43b50cdae6be58c722603ccc08d3e
ShreyaDAug19 <shreyadubey1897@gmail.com>
2019-09-15 21:30:01 +0530

    adding pattern

---
+++ src/ShreyaD/P8.java
@@ -0,0 +1,45 @@
+package ShreyaD;
+
+import java.util.Scanner;
+public class P8 {
+
+	void pattern(int n)
+	{
+		for(int i=1;i<=2*n-1;i++)
+		{
+			if(i<=n){
+			for(int j=1;j<=n-i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++)
+			{
+				System.out.print("* ");
+			}
+			}
+			else
+			{
+				for(int j=1;j<=i-n;j++)
+				{
+					System.out.print(" ");
+				}
+				for(int k=2*n-1;k>=i;k--)
+				{
+					System.out.print("* ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		P8 p8 = new P8();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the no of max rows");
+		int n = sc.nextInt();
+		p8.pattern(n);
+		sc.close();
+
+	}
+
+}
