28eace3d34304d5abea692a0125a70ba63fe0923
Atul Shrivastava <ashrivastava14@gmail.com>
2019-09-15 08:53:10 +0530

    Modified pattern1 program

--- src/AtulS/Pattern1.java
+++ src/AtulS/Pattern1.java
@@ -9,8 +9,9 @@ package AtulS;
 public class Pattern1 {
 
 	public static void main(String[] args) {
-		for (int i = 1; i <= 5; i++) {
-			for (int j = 5; j >= i; j--) {
+		int row=5;
+		for (int i = 1; i <= row; i++) {
+			for (int j = row; j >= i+1; j--) {
 				System.out.print(" ");
 			}
 			for (int k = 1; k <= i; k++) {
