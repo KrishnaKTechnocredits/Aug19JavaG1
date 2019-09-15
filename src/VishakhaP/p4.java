7506a369145c6da9b815a8ab0fbe192352d9f117
VishakhapAug19 <vishpagar28@gmail.com>
2019-09-15 19:17:39 +0530

    adding latest file

---
+++ src/VishakhaP/p4.java
@@ -0,0 +1,20 @@
+package VishakhaP;
+
+public class p4 {
+	public static void main(String args[])
+	{
+		for(int i =1;i<=4 ;i++)
+		{
+			for(int j = 1;j<= i;j++)
+			{
+				System.out.print("* ");
+			}
+			    System.out.println("") ;  
+		}
+	}
+
+}
+
+
+
+
