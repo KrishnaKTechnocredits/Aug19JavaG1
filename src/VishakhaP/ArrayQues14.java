09f6cd861be6ac7bd6c73b16cd67f8315bbced8a
VishakhapAug19 <vishpagar28@gmail.com>
2019-09-28 22:56:47 +0530

    one more array program

---
+++ src/VishakhaP/ArrayQues14.java
@@ -0,0 +1,18 @@
+package VishakhaP;
+
+public class ArrayQues14 {
+	public static void main(String args[])
+	{
+		int arr[]={15,24,52,49,62,10};
+		System.out.println("Even Number : ");
+		for (int i=0;i<=arr.length;i++)
+		{
+			if (arr[i]%2==0)
+			{
+				System.out.println(arr[i]);
+			}
+		}
+				
+	}
+
+}
