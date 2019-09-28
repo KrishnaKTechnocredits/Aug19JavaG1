0e5cbbf9f7feaf75a8340522037f2f9f4161cab5
VishakhapAug19 <vishpagar28@gmail.com>
2019-09-28 23:01:18 +0530

    array assignment pgm

---
+++ src/VishakhaP/ArrayQues6.java
@@ -0,0 +1,16 @@
+package VishakhaP;
+
+import java.util.Arrays;
+
+public class ArrayQues6 {
+	public static void main(String args[])
+	{
+		int a[]= {1,2,3,4,5};
+		int b[]= {1,2,3,4,5};
+		System.out.println(Arrays.equals(a, b));
+				
+		}
+		
+	}
+
+
