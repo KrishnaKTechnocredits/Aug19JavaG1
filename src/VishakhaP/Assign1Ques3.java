2e562f0cef5acc4b762ff2ea44cf7437b07d9372
VishakhapAug19 <vishpagar28@gmail.com>
2019-09-29 09:15:50 +0530

    Assignment 1

---
+++ src/VishakhaP/Assign1Ques3.java
@@ -0,0 +1,18 @@
+package VishakhaP;
+
+public class Assign1Ques3 {
+	public static void main (String args[]){
+		int arr[] = {6,3,8};
+		int sum = 0;
+		for(int i =0;i<arr.length;i++)
+		{
+			sum = sum + arr[i];
+		}
+		
+				System.out.println("sum of all element :" +sum);
+		
+				
+				
+	}
+
+}
